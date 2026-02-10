1438c2ac6f845f4b1658f4f095281b329db3e0e6
Clifford Nwanna <Clifford.nwanna@wemabank.com>
2026-02-10 18:18:16 +0100

    fic

--- ingest.py
+++ ingest.py
@@ -1,6 +1,6 @@
 import os
 from pathlib import Path
-from langchain.document_loaders import PyPDFLoader, TextLoader
+from langchain_community.document_loaders import PyPDFLoader, TextLoader
 from langchain_community.embeddings import OllamaEmbeddings
 from langchain_community.vectorstores import Chroma
 from langchain.text_splitter import RecursiveCharacterTextSplitter
